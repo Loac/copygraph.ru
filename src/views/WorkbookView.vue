59fe9aa5074fd4a3e16cfa9df2d3f97fd3c289f8
Dmitry Loac <reloac@gmail.com>
2023-12-31 03:45:46 +0300

    expansion panel

--- src/views/WorkbookView.vue
+++ src/views/WorkbookView.vue
@@ -10,108 +10,124 @@
       <v-list-item title="Navigation drawer"></v-list-item>
 
       <v-form @submit.prevent>
-        <v-text-field
-          v-model.number="count"
-          label="Count"
-          type="number"
-        ></v-text-field>
-        <v-text-field
-          v-model="fence.fenceHeight"
-          label="Fence height"
-          type="number"
-          @input="fence.update()"
-        ></v-text-field>
-        <v-text-field
-          v-model="fence.fenceMargin"
-          label="Fence space"
-          type="number"
-          :rules="rules"
-          @input="fence.update()"
-        ></v-text-field>
-        <v-text-field
-          v-model="fence.fractions"
-          label="Fractions"
-          type="number"
-          @input="fence.update()"
-        ></v-text-field>
-
-
-        <div class="d-flex pa-5 align-end">
-          <v-menu :close-on-content-click="false" :location="'center'">
-            <template v-slot:activator="{ props }">
+        <v-expansion-panels
+          variant="accordion"
+          :multiple="true">
+          <v-expansion-panel elevation="1">
+            <v-expansion-panel-title>Fence</v-expansion-panel-title>
+            <v-expansion-panel-text>
               <v-text-field
-                label="Width"
-                variant="underlined"
-                hide-details
-                v-bind="props"
-                v-model="fence.baseBorderWidth"
-                :readonly="true"
-                @input="fence.update()"
-                style="cursor: pointer"
+                v-model.number="count"
+                label="Count"
+                type="number"
               ></v-text-field>
-            </template>
-            <v-card>
-              <v-card-text class="pa-5">
-                <v-slider
-                  v-model="fence.baseBorderWidth"
-                  direction="vertical"
-                  :min="0"
-                  :max="5"
-                  :step="1"
-                ></v-slider>
-              </v-card-text>
-            </v-card>
-          </v-menu>
+              <v-text-field
+                v-model="fence.fenceHeight"
+                label="Fence height"
+                type="number"
+                @input="fence.update()"
+              ></v-text-field>
+              <v-text-field
+                v-model="fence.fenceMargin"
+                label="Fence space"
+                type="number"
+                :rules="rules"
+                @input="fence.update()"
+              ></v-text-field>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
 
-          <v-select
-            label="Style"
-            variant="underlined"
-            hide-details
-            v-model="fence.baseBorderStyle"
-            :items="[{
-              title: 'Dotted',
-              value: 'dotted'
-            }, {
-              title: 'Dashed',
-              value: 'dashed'
-            }, {
-              title: 'Solid',
-              value: 'solid'
-            }]"
-          ></v-select>
+          <v-expansion-panel elevation="1">
+            <v-expansion-panel-title>Fractions</v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <v-text-field
+                v-model="fence.fractions"
+                label="Fractions"
+                type="number"
+                @input="fence.update()"
+              ></v-text-field>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
 
-          <v-menu :close-on-content-click="false">
-            <template v-slot:activator="{ props }">
-              <v-btn v-bind="props" :style="swatchStyle(fence.baseBorderColor)" elevation="0"></v-btn>
-            </template>
-            <v-card>
-              <v-card-text class="pa-0">
-                <v-color-picker v-model="fence.baseBorderColor"
-                                :hide-inputs="true"
-                                :modes="['rgb']"
-                                flat />
-              </v-card-text>
-            </v-card>
-          </v-menu>
-        </div>
+          <v-expansion-panel elevation="1">
+            <v-expansion-panel-title>Base</v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <div class="d-flex align-end">
+                <v-menu :close-on-content-click="false" :location="'center'">
+                  <template v-slot:activator="{ props }">
+                    <v-text-field
+                      label="Width"
+                      variant="underlined"
+                      hide-details
+                      v-bind="props"
+                      v-model="fence.baseBorderWidth"
+                      :readonly="true"
+                      @input="fence.update()"
+                      style="cursor: pointer"
+                    ></v-text-field>
+                  </template>
 
+                  <v-card>
+                    <v-card-text class="pa-5">
+                      <v-slider
+                        v-model="fence.baseBorderWidth"
+                        direction="vertical"
+                        :min="0"
+                        :max="5"
+                        :step="1"
+                      ></v-slider>
+                    </v-card-text>
+                  </v-card>
+                </v-menu>
 
+                <v-select
+                  label="Style"
+                  variant="underlined"
+                  hide-details
+                  v-model="fence.baseBorderStyle"
+                  :items="[{
+                      title: 'Dotted',
+                      value: 'dotted'
+                    }, {
+                      title: 'Dashed',
+                      value: 'dashed'
+                    }, {
+                      title: 'Solid',
+                      value: 'solid'
+                    }]"
+                  ></v-select>
 
+                <v-menu :close-on-content-click="false">
+                  <template v-slot:activator="{ props }">
+                    <v-btn v-bind="props" :style="swatchStyle(fence.baseBorderColor)" elevation="0"></v-btn>
+                  </template>
+                  <v-card>
+                    <v-card-text class="pa-0">
+                      <v-color-picker v-model="fence.baseBorderColor"
+                                      :hide-inputs="true"
+                                      :modes="['rgb']"
+                                      flat />
+                    </v-card-text>
+                  </v-card>
+                </v-menu>
+              </div>
 
+              <v-text-field
+                v-model="fence.basePosition"
+                label="Base position"
+                type="number"
+                @input="fence.update()"
+              ></v-text-field>
+              <v-text-field
+                v-model="fence.baseCoverage"
+                label="Base coverage"
+                type="number"
+                @input="fence.update()"
+              ></v-text-field>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
 
-
-        <v-text-field
-          v-model="fence.basePosition"
-          label="Base position"
-          type="number"
-          @input="fence.update()"
-        ></v-text-field>
-        <v-text-field
-          v-model="fence.baseCoverage"
-          label="Base coverage"
-          type="number"
-          @input="fence.update()"
-        ></v-text-field>
 
         <v-btn type="submit" block class="mt-2" @click="someMethod">Submit</v-btn>
       </v-form>
