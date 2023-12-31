48f00d7636e770e1cf01b707d940828d3ba9ed58
Dmitry Loac <reloac@gmail.com>
2023-12-31 11:52:30 +0300

    ColorPickerDiv

---
+++ src/components/ColorPickerDiv.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-menu :close-on-content-click="false">
+    <template v-slot:activator="{ props }">
+      <v-btn
+        v-bind="props"
+        v-model="model"
+        :style="swatchStyle(model)"
+        elevation="0"
+      ></v-btn>
+    </template>
+    <v-card>
+      <v-card-text class="pa-0">
+        <v-color-picker
+          v-model="model"
+          :hide-inputs="true"
+          :modes="['rgb']"
+          flat
+        />
+      </v-card-text>
+    </v-card>
+  </v-menu>
+</template>
+
+<script setup lang="ts">
+  import { defineModel } from "vue";
+
+  const model = defineModel();
+
+  const swatchStyle = function(model: any) {
+      return {
+        color: model,
+        cursor: 'pointer',
+      }
+  }
+</script>
