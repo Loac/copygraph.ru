79e12a2cc768d1b76edba5bc7cecf9bf843ee77f
Dmitry Loac <reloac@gmail.com>
2024-01-03 06:27:37 +0300

    misc

--- src/components/copygraph/WorkbookAdvanced.ts
+++ src/components/copygraph/WorkbookAdvanced.ts
@@ -1,6 +1,5 @@
 import { LineStyle } from "@/components/copygraph/Copygraph";
-import {th} from "vuetify/locale";
-import {StyleValue} from "vue";
+import { StyleValue } from "vue";
 
 export class Workbook {
     fractionHeight: number = 3;
@@ -11,48 +10,48 @@ export class Workbook {
     layers: Array<Layer> = [];
 
     removeLayer(layer: Layer): void {
-      const index: number = this.layers.indexOf(layer);
-      if (index !== -1) {
-        this.layers.splice(index, 1);
-      }
+        const index: number = this.layers.indexOf(layer);
+        if (index !== -1) {
+            this.layers.splice(index, 1);
+        }
     }
 
     showLayer(layer: Layer): void {
-      const index: number = this.layers.indexOf(layer);
-      if (index !== -1) {
-        this.layers[index].visible = !this.layers[index].visible;
-      }
+        const index: number = this.layers.indexOf(layer);
+        if (index !== -1) {
+            this.layers[index].visible = !this.layers[index].visible;
+        }
     }
 
     newLayer(): Layer {
-      const layer: Layer = new Layer();
-      layer.offset = 6;
-      layer.rhythm = [8,2,2,2,2];
-      layer.lineStyle.width = 1;
-      layer.lineStyle.style = 'dashed';
-      layer.lineStyle.color = '#AAAAAA';
-      return layer;
+        const layer: Layer = new Layer();
+        layer.offset = 6;
+        layer.rhythm = [8,2,2,2,2];
+        layer.lineStyle.width = 1;
+        layer.lineStyle.style = 'dashed';
+        layer.lineStyle.color = '#AAAAAA';
+        return layer;
     }
 
     addNewLayer(): void {
-      this.layers.push(this.newLayer());
+        this.layers.push(this.newLayer());
     }
 
     addLayer(layer: Layer): void {
-      this.layers.push(layer);
+        this.layers.push(layer);
     }
 
     layerStyle(offset: number): StyleValue {
-      return {
-        marginTop: (this.fractionHeight * offset - this.fractionHeight) * -1 + 'mm'
-      }
+        return {
+          marginTop: (this.fractionHeight * offset - this.fractionHeight) * -1 + 'mm'
+        }
     }
     lineStyle(offset: number, lineStyle: LineStyle): StyleValue {
-      return {
-        height: this.fractionHeight + 'mm',
-        marginTop: (this.fractionHeight * offset - this.fractionHeight) + 'mm',
-        borderTop: lineStyle.width + 'px ' + lineStyle.style + ' ' + lineStyle.color
-      }
+        return {
+            height: this.fractionHeight + 'mm',
+            marginTop: (this.fractionHeight * offset - this.fractionHeight) + 'mm',
+            borderTop: lineStyle.width + 'px ' + lineStyle.style + ' ' + lineStyle.color
+        }
     }
 }
 
