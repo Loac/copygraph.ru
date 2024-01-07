7d9a847c78ef1551f7068aaaa77e52def58d7467
Dmitry Loac <reloac@gmail.com>
2024-01-07 08:12:22 +0300

    refactor LineStyleField

---
+++ src/components/fields/LineStyleField.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="d-flex align-end">
+        <NumberPicker
+            v-model="model.width"
+            label="Width"
+            :min="0"
+            :max="5"
+            :step="1"
+        ></NumberPicker>
+
+        <StylePicker
+            label="Style"
+            v-model="model.style"
+        ></StylePicker>
+
+        <ColorPicker
+            variant="underlined"
+            hide-details
+            v-model="model.color"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import StylePicker from "@/components/StylePicker.vue";
+    import NumberPicker from "@/components/NumberPicker.vue";
+    import ColorPicker from "@/components/ColorPicker.vue";
+    import { defineModel } from "vue";
+    import { LineStyle } from "@/components/copygraph/Copygraph";
+
+    const model = defineModel<LineStyle>({
+        required: true
+    });
+</script>
