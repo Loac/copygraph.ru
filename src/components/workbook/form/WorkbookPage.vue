95deeb2b433b51eb834bad1c6c9b5d35d9254f0f
Dmitry Loac <reloac@gmail.com>
2024-01-13 12:23:20 +0300

    split WorkbookPage

---
+++ src/components/workbook/form/WorkbookPage.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-card variant="flat">
+        <v-card-text>
+            <div class="d-flex ga-4">
+                <v-text-field
+                    v-model.number="copygraph.workbook.pageWidth"
+                    label="Width"
+                    variant="underlined"
+                    suffix="mm"
+                    hide-details
+                    :readonly="true"
+                    :disabled="true"
+                />
+                <v-text-field
+                    v-model.number="copygraph.workbook.pageHeight"
+                    label="Height"
+                    variant="underlined"
+                    suffix="mm"
+                    hide-details
+                    :readonly="true"
+                    :disabled="true"
+                />
+            </div>
+            <div class="d-flex mt-4 ga-4">
+                <NumberPicker
+                    v-model.number="copygraph.workbook.pagePadding"
+                    label="Padding"
+                    suffix="mm"
+                    :min="0"
+                    :max="20"
+                    :step="0.1"
+                />
+                <NumberPicker
+                    v-model.number="copygraph.workbook.fractionHeight"
+                    label="Fraction"
+                    suffix="mm"
+                    :min="1"
+                    :max="10"
+                    :step="0.01"
+                />
+            </div>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+    import { ModelRef } from "vue";
+    import { Copygraph } from "@/components/copygraph/Copygraph";
+    import NumberPicker from "@/components/fields/NumberPickerField.vue";
+
+    const copygraph:ModelRef<Copygraph> = defineModel({ required: true });
+</script>
--- src/views/WorkbookView.vue
+++ src/views/WorkbookView.vue
@@ -26,48 +26,7 @@
             <v-expansion-panel elevation="0">
                 <v-expansion-panel-title>Page</v-expansion-panel-title>
                 <v-expansion-panel-text>
-                    <v-card variant="flat">
-                        <v-card-text>
-                            <div class="d-flex ga-4">
-                                <v-text-field
-                                    v-model.number="copygraph.workbook.pageWidth"
-                                    label="Width"
-                                    variant="underlined"
-                                    suffix="mm"
-                                    hide-details
-                                    :readonly="true"
-                                    :disabled="true"
-                                />
-                                <v-text-field
-                                    v-model.number="copygraph.workbook.pageHeight"
-                                    label="Height"
-                                    variant="underlined"
-                                    suffix="mm"
-                                    hide-details
-                                    :readonly="true"
-                                    :disabled="true"
-                                />
-                            </div>
-                            <div class="d-flex mt-4 ga-4">
-                                <NumberPicker
-                                    v-model.number="copygraph.workbook.pagePadding"
-                                    label="Padding"
-                                    suffix="mm"
-                                    :min="0"
-                                    :max="20"
-                                    :step="0.1"
-                                />
-                                <NumberPicker
-                                    v-model.number="copygraph.workbook.fractionHeight"
-                                    label="Fraction"
-                                    suffix="mm"
-                                    :min="1"
-                                    :max="10"
-                                    :step="0.01"
-                                />
-                            </div>
-                        </v-card-text>
-                    </v-card>
+                    <WorkbookPage v-model="copygraph" />
                 </v-expansion-panel-text>
             </v-expansion-panel>
             <v-expansion-panel elevation="0">
@@ -170,6 +129,7 @@
     import { printToPdf } from "@/components/copygraph/Utils";
     import { Copygraph } from "@/components/copygraph/Copygraph";
     import WorkbookPreset from "@/components/workbook/form/WorkbookPreset.vue";
+    import WorkbookPage from "@/components/workbook/form/WorkbookPage.vue";
 
     const copygraph = ref(new Copygraph());
     // Раскрыть первую панель.
