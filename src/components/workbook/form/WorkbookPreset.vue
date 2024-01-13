e62d2ed0e502a06158cd5eabca91a948eeee5580
Dmitry Loac <reloac@gmail.com>
2024-01-13 12:17:12 +0300

    split WorkbookPreset

---
+++ src/components/workbook/form/WorkbookPreset.vue
@@ -0,0 +1,136 @@
+<template>
+    <v-card variant="flat">
+        <v-card-text>
+            <v-select
+                v-model="copygraph.activePresetName"
+                label="Preset"
+                item-title="name"
+                variant="underlined"
+                hide-details
+                :items="listPreset()"
+                @update:modelValue="acceptPreset"
+            />
+        </v-card-text>
+        <v-card-actions>
+            <v-btn
+                rounded="xs"
+                size="small"
+                variant="text"
+                text="Save"
+                color="blue"
+                prepend-icon="mdi-book-outline"
+                @click="savePreset()"
+            />
+            <v-btn
+                rounded="xs"
+                size="small"
+                variant="text"
+                text="Download"
+                color="blue"
+                prepend-icon="mdi-download"
+                @click="downloadPreset()"
+            />
+            <v-btn
+                rounded="xs"
+                size="small"
+                variant="text"
+                text="Load"
+                color="blue"
+                prepend-icon="mdi-upload"
+                @click="uploadPresetDialog()"
+            />
+            <v-file-input
+                v-model="selectFile"
+                id="uploadPreset"
+                accept=".json"
+                class="d-none"
+                @change="uploadPreset"
+            />
+        </v-card-actions>
+    </v-card>
+
+    <v-snackbar v-model="presetSaveSnackbar" :timeout="3000">
+        Preset are saved in cookies and will be available only you.
+    </v-snackbar>
+    <v-snackbar v-model="presetParseError" color="red" :timeout="3000">
+        <div class="text-center">Wrong preset format.</div>
+    </v-snackbar>
+</template>
+
+<script setup lang="ts">
+    import { ModelRef, onMounted, ref } from "vue";
+    import { Copygraph } from "@/components/copygraph/Copygraph";
+    import { download } from "@/components/copygraph/Utils";
+    import { useCookies } from "vue3-cookies";
+
+    const copygraph:ModelRef<Copygraph> = defineModel({ required: true });
+    const { cookies } = useCookies();
+    const presetSaveSnackbar = ref(false);
+    const presetParseError = ref(false);
+    const selectFile = ref(null);
+
+    const listPreset = (): Array<string> => {
+        return copygraph.value.listPreset();
+    }
+
+    const savePreset = (): void => {
+        try {
+            copygraph.value.savePreset('[Custom]');
+            cookies.set('preset', copygraph.value.exportPresetToJson('[Custom]'));
+            presetSaveSnackbar.value = true;
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const downloadPreset = (): void => {
+        download(copygraph.value.extractPreset('[Custom]'), 'preset.json');
+    }
+
+    const uploadPresetDialog = (): void => {
+        let element: HTMLElement | null = document.getElementById("uploadPreset");
+        if (null != element) {
+            element.click()
+        }
+    }
+
+    const uploadPreset = (): void => {
+        if (null == selectFile.value) {
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.readAsText(selectFile.value[0]);
+        reader.onload = () => {
+            if (typeof reader.result == 'string'
+                && !copygraph.value.acceptPresetFromJson(reader.result)) {
+                presetParseError.value = true;
+            }
+            selectFile.value = null;
+        }
+    }
+
+    const loadPreset = (): void => {
+        copygraph.value.addPresetFromData(cookies.get('preset'));
+    }
+
+    /**
+     * Костыль `workbook.fractionHeight`. Не понимаю, как еще
+     * заставить Vue перерисовать компонент.
+     */
+    const acceptPreset = (presetName: string): void => {
+        copygraph.value.workbook.fractionHeight = 1;
+        copygraph.value.acceptPresetByName(presetName);
+    }
+
+    onMounted(() => {
+        new Promise(() => {
+            copygraph.value.buildPresets().then((): void => {
+                loadPreset();
+                copygraph.value.workbook.fractionHeight = 1;
+                copygraph.value.acceptPresetByName('A4 6x6');
+                listPreset();
+            });
+        });
+    });
+</script>
--- src/views/WorkbookView.vue
+++ src/views/WorkbookView.vue
@@ -11,59 +11,16 @@
             <v-btn @click="printToPdf" text="PDF" />
         </v-toolbar>
 
-        <v-expansion-panels variant="accordion" class="no-padding" :multiple="true">
-            <v-expansion-panel elevation="0">
+        <v-expansion-panels
+            v-model="panel"
+            variant="accordion"
+            class="no-padding"
+            :multiple="true"
+        >
+            <v-expansion-panel elevation="0" :aria-expanded="true">
                 <v-expansion-panel-title>Preset</v-expansion-panel-title>
                 <v-expansion-panel-text>
-                    <v-card variant="flat">
-                        <v-card-text>
-                            <v-select
-                                v-model="copygraph.activePresetName"
-                                label="Preset"
-                                item-title="name"
-                                variant="underlined"
-                                hide-details
-                                :items="listPreset()"
-                                @update:modelValue="acceptPreset"
-                            />
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-btn
-                                rounded="xs"
-                                size="small"
-                                variant="text"
-                                text="Save"
-                                color="blue"
-                                prepend-icon="mdi-book-outline"
-                                @click="savePreset()"
-                            />
-                            <v-btn
-                                rounded="xs"
-                                size="small"
-                                variant="text"
-                                text="Download"
-                                color="blue"
-                                prepend-icon="mdi-download"
-                                @click="downloadPreset()"
-                            />
-                            <v-btn
-                                rounded="xs"
-                                size="small"
-                                variant="text"
-                                text="Load"
-                                color="blue"
-                                prepend-icon="mdi-upload"
-                                @click="uploadPresetDialog()"
-                            />
-                            <v-file-input
-                                v-model="selectFile"
-                                id="uploadPreset"
-                                accept=".json"
-                                class="d-none"
-                                @change="uploadPreset"
-                            />
-                        </v-card-actions>
-                    </v-card>
+                    <WorkbookPreset v-model="copygraph" />
                 </v-expansion-panel-text>
             </v-expansion-panel>
             <v-expansion-panel elevation="0">
@@ -202,13 +159,6 @@
             </v-expansion-panel>
         </v-expansion-panels>
     </v-navigation-drawer>
-
-    <v-snackbar v-model="presetSaveSnackbar" :timeout="3000">
-        Preset are saved in cookies and will be available only you.
-    </v-snackbar>
-    <v-snackbar v-model="presetParseError" color="red" :timeout="3000">
-        <div class="text-center">Wrong preset format.</div>
-    </v-snackbar>
 </template>
 
 <script setup lang="ts">
@@ -216,79 +166,12 @@
     import WorkbookRhythm from "@/components/fields/WorkbookRhythmField.vue";
     import LineStyleField from "@/components/fields/LineStyleField.vue";
     import NumberPicker from "@/components/fields/NumberPickerField.vue";
-    import { onMounted, ref } from "vue";
-    import { download, printToPdf } from "@/components/copygraph/Utils";
-    import { useCookies } from "vue3-cookies";
+    import { ref } from "vue";
+    import { printToPdf } from "@/components/copygraph/Utils";
     import { Copygraph } from "@/components/copygraph/Copygraph";
+    import WorkbookPreset from "@/components/workbook/form/WorkbookPreset.vue";
 
-    const { cookies } = useCookies();
     const copygraph = ref(new Copygraph());
-    const presetSaveSnackbar = ref(false);
-    const presetParseError = ref(false);
-    const selectFile = ref(null);
-
-    const downloadPreset = (): void => {
-        download(copygraph.value.extractPreset('[Custom]'), 'preset.json');
-    }
-
-    const uploadPresetDialog = (): void => {
-        let element: HTMLElement | null = document.getElementById("uploadPreset");
-        if (null != element) {
-            element.click()
-        }
-    }
-
-    const uploadPreset = (): void => {
-        if (null == selectFile.value) {
-            return;
-        }
-
-        const reader = new FileReader();
-        reader.readAsText(selectFile.value[0]);
-        reader.onload = () => {
-            if (typeof reader.result == 'string'
-                && !copygraph.value.acceptPresetFromJson(reader.result)) {
-                presetParseError.value = true;
-            }
-            selectFile.value = null;
-        }
-    }
-
-    const savePreset = (): void => {
-        try {
-            copygraph.value.savePreset('[Custom]');
-            cookies.set('preset', copygraph.value.exportPresetToJson('[Custom]'));
-            presetSaveSnackbar.value = true;
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    const loadPreset = (): void => {
-        copygraph.value.addPresetFromData(cookies.get('preset'));
-    }
-
-    const listPreset = (): Array<string> => {
-        return copygraph.value.listPreset();
-    }
-
-    /**
-     * Костыль `workbook.fractionHeight`. Не понимаю, как еще
-     * заставить Vue перерисовать компонент.
-     */
-    const acceptPreset = (presetName: string): void => {
-        copygraph.value.workbook.fractionHeight = 1;
-        copygraph.value.acceptPresetByName(presetName);
-    }
-
-    onMounted(() => {
-        new Promise(() => {
-            copygraph.value.buildPresets().then((): void => {
-                loadPreset();
-                copygraph.value.workbook.fractionHeight = 1;
-                copygraph.value.acceptPresetByName('A4 6x6');
-                listPreset();
-            });
-        });
-    });
+    // Раскрыть первую панель.
+    const panel = ref([0]);
 </script>
