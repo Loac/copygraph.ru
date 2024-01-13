9e52507bc4a2430533e8d7db98f7fa71ede584f2
Dmitry Loac <reloac@gmail.com>
2024-01-13 11:06:45 +0300

    misc

--- src/components/workbook/form/WorkbookRhythm.vue
+++ src/components/workbook/form/WorkbookRhythm.vue
@@ -1,32 +1,28 @@
 <template>
-  <v-text-field
-    v-model="model"
-    variant="underlined"
-    hide-details
-    :label="label"
-  ></v-text-field>
+    <v-text-field
+        v-model="model"
+        variant="underlined"
+        hide-details
+        :label="label"
+    ></v-text-field>
 </template>
 
 <script setup lang="ts">
-  import {defineModel, defineProps} from "vue";
+import {defineModel, defineProps} from "vue";
 
-  const [model, modifiers] = defineModel({
+const [model, modifiers] = defineModel({
     set(value: string) {
-      if (modifiers.rhythm) {
-        return value.split(',').map((item) => Number(item));
-      }
-      return value
+        if (modifiers.rhythm) {
+            return value.split(',').map((item) => Number(item));
+        }
+        return value
     }
-  });
+});
 
-  defineProps({
+defineProps({
     label: {
-      type: String,
-      default: ''
+        type: String,
+        default: ''
     },
-  });
+});
 </script>
-
-<style scoped>
-
-</style>
